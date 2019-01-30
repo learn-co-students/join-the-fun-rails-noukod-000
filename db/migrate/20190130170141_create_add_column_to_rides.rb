a0e7b3b462a7dc80b089cf6be64d79c8cbf44edb
Merline Belizaire <merline@noukod.com>
2019-01-30 17:14:06 +0000

    Done.

--- db/migrate/20190130170141_create_add_column_to_rides.rb
+++
@@ -1,8 +0,0 @@
-class CreateAddColumnToRides < ActiveRecord::Migration
-  def change
-    create_table :add_column_to_rides do |t|
-        add_column :rides, :passenger_id, :integer
-        add_column :rides, :taxi_id, :integer
-    end
-  end
-end
