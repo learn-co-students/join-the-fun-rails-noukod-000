7600f48f6862c532ff5a14786bfa19cbd3e517f1
Herna Michel <labellehernamichel@yahoo.fr>
2019-07-07 23:02:43 +0000

    Done.

--- db/migrate/20140212205242_create_rides.rb
+++ db/migrate/20140212205242_create_rides.rb
@@ -1,7 +1,11 @@
 class CreateRides < ActiveRecord::Migration
   def change
     create_table :rides do |t|
-      t.timestamps null: false
+      t.integer :passenger_id
+       t.integer :taxi_id
+       t.datetime :ride_date
+
+       t.timestamps null: false
     end
   end
 end
